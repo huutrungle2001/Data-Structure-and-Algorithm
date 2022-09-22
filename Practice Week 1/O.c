119bdb5ed4e8afa8583bc7cf1d9676eb378def76
huutrungle2001 <huutrungle2001@gmail.com>
2022-09-22 16:19:30 +0700

    Finish O

--- Practice Week 1/O.c
+++ Practice Week 1/O.c
@@ -3,13 +3,23 @@
 long long fib[1000000] = {0};
 
 long long getFib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return 1;
+    }
+
     if (fib[n] != 0) {
         return fib[n];
     }
+
     return fib[n] = getFib(n - 1) + getFib(n - 2);
 }
 
 int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%lld", getFib(n));
 }
